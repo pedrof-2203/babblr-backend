c10dd289095973603e6d5764c80dfafe53fd1015
pedro <pedroantoniofpa22@gmail.com>
2026-09-23 16:25:19 -0300

    feat(users): add GraphQL user CRUD with password hashing

---
+++ src/users/users.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { AbstractRepository } from '../common/database/abstract.repository';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersRepository extends AbstractRepository<User> {
+  protected readonly logger = new Logger(UsersRepository.name);
+
+  constructor(
+    @InjectModel(User.name)
+    userModel: Model<User>,
+  ) {
+    super(userModel);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async create(createUserInput: CreateUserInput) {
+    return this.usersRepository.create({
+      ...createUserInput,
+      password: await this.hashPassword(createUserInput.password),
+    });
+  }
+
+  async findAll() {
+    return this.usersRepository.findMany({});
+  }
+
+  async findOne(_id: string) {
+    return this.usersRepository.findOne({ _id });
+  }
+
+  async update(_id: string, updateUserInput: UpdateUserInput) {
+    if (updateUserInput.password) {
+      updateUserInput.password = await this.hashPassword(
+        updateUserInput.password,
+      );
+    }
+
+    return this.usersRepository.findOneAndUpdate(
+      { _id },
+      {
+        $set: {
+          ...updateUserInput,
+        },
+      },
+    );
+  }
+
+  async remove(_id: string) {
+    return this.usersRepository.findOneAndDelete({ _id });
+  }
+
+  private async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+}
